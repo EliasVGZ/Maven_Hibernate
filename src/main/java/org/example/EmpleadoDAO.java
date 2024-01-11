a2edda9ce738f585831518f1ab53017e8d13e653
Elias <elias.bs2@hotmail.com>
2024-01-11 12:38:33 +0100

    mas funciones!

--- src/main/java/org/example/EmpleadoDAO.java
+++ src/main/java/org/example/EmpleadoDAO.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
 
@@ -26,6 +27,65 @@ public class EmpleadoDAO {
         } catch (Exception e) {
             e.printStackTrace();
             return null;
+        }
+    }
+
+    public List<Empleado> obtenerEmpleadosConSalarioMayorDe3000() {
+        try (Session session = sessionFactory.openSession()) {
+            // Creamos la consulta HQL para obtener empleados con salario mayor de 3000
+            String hql = "FROM Empleado WHERE salario > 3000";
+            Query<Empleado> query = session.createQuery(hql, Empleado.class);
+
+            // Ejecutamos la consulta y devolvemos los resultados
+            return query.list();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public List<Empleado> obtenerEmpleadosConSalarioMayorA(double salarioMinimo) {
+        try (Session session = sessionFactory.openSession()) {
+            // Creamos la consulta HQL para obtener empleados con salario mayor al proporcionado
+            String hql = "FROM Empleado WHERE salario > :salarioMinimo";
+            Query<Empleado> query = session.createQuery(hql, Empleado.class);
+            query.setParameter("salarioMinimo", salarioMinimo);
+
+            // Ejecutamos la consulta y devolvemos los resultados
+            return query.list();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void actualizarSalarioPorNIF(String nif, double nuevoSalario) {
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = null;
+
+            try {
+                transaction = session.beginTransaction();
+
+                // Obtener el empleado por NIF
+                Empleado empleado = session.get(Empleado.class, nif);
+
+                if (empleado != null) {
+                    // Actualizar el salario
+                    empleado.setSalario(nuevoSalario);
+
+                    // Guardar los cambios
+                    session.update(empleado);
+                    transaction.commit();
+                    System.out.println("Salario actualizado con éxito.");
+                } else {
+                    System.out.println("No se encontró un empleado con el NIF proporcionado.");
+                }
+            } catch (Exception e) {
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+                e.printStackTrace();
+            }
         }
     }
 }
--- src/main/java/org/example/MainApp.java
+++ src/main/java/org/example/MainApp.java
@@ -1,13 +1,19 @@
 package org.example;
 
+import jdk.swing.interop.SwingInterOpUtils;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 
 public class MainApp {
 
-    public static void main(String[] args) {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
         // Configuración de Hibernate y creación de la sesión
         Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
         SessionFactory sessionFactory = configuration.buildSessionFactory();
@@ -17,6 +23,8 @@ public class MainApp {
 
         // Obtención de todos los empleados
         List<Empleado> empleados = empleadoDAO.obtenerTodosLosEmpleados();
+        List<Empleado> empleadosSalarioSuperior3000 = empleadoDAO.obtenerEmpleadosConSalarioMayorDe3000();
+
 
         // Imprimir los empleados
         if (empleados != null) {
@@ -27,6 +35,50 @@ public class MainApp {
                 System.out.println("Salario: " + empleado.getSalario());
                 System.out.println("------------------------");
             }
+        }
+
+        //TODO EMPLEADOS CON SALARIO SUPERIOR A 3000 EUROS
+
+//        if(empleadosSalarioSuperior3000 != null){
+//
+//            System.out.println("Empleados con salario superior a 3000 euros");
+//            for (Empleado empleado : empleadosSalarioSuperior3000) {
+//                System.out.println("NIF: " + empleado.getNif());
+//                System.out.println("Nombre: " + empleado.getNombre());
+//                System.out.println("Apellidos: " + empleado.getApellidos());
+//                System.out.println("Salario: " + empleado.getSalario());
+//                System.out.println("------------------------");
+//            }
+//        }
+
+//        System.out.print("Ingrese un salario mínimo: ");
+//        double salarioMinimo = Double.parseDouble(br.readLine());
+//
+//        // Obtención de empleados con salario mayor al proporcionado por el usuario
+//        List<Empleado> empleadosConSalarioMayorAlUsuario = empleadoDAO.obtenerEmpleadosConSalarioMayorA(salarioMinimo);
+//
+//        // Imprimir los resultados
+//        if (empleadosConSalarioMayorAlUsuario != null) {
+//            for (Empleado empleado : empleadosConSalarioMayorAlUsuario) {
+//                System.out.println("NIF: " + empleado.getNif());
+//                System.out.println("Nombre: " + empleado.getNombre());
+//                System.out.println("Apellidos: " + empleado.getApellidos());
+//                System.out.println("Salario: " + empleado.getSalario());
+//                System.out.println("------------------------");
+//            }
+//        }
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.print("Ingrese el NIF del empleado: ");
+            String nif = reader.readLine();
+
+            System.out.print("Ingrese el nuevo salario: ");
+            double nuevoSalario = Double.parseDouble(reader.readLine());
+
+            // Llamar al método para actualizar el salario por NIF
+            empleadoDAO.actualizarSalarioPorNIF(nif, nuevoSalario);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
         // Cierre de la sesión de Hibernate
